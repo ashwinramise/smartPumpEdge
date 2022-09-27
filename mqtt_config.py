612956114de5c52e3c067965919500d224d55141
Ashwin Ram <72587716+ashwinramise@users.noreply.github.com>
2022-09-27 09:14:40 -0500

    Update mqtt_config.py

--- mqtt_config.py
+++ mqtt_config.py
@@ -1,6 +1,6 @@
 # MQTT defenitions
 mqtt_client = 'BuckmanDigital'
 mqtt_broker = 'broker.hivemq.com'
-pumpName = 'ddapump1'
+pumpName = 'DH_dda001'
 Location = 'DigitalHub'
 domain = 'buckman/ackumen/smartAssets/'
