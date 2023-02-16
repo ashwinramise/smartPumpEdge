763da0edb7ec42711b2397282ef7979d0eb92c93
Ashwin Ram <72587716+ashwinramise@users.noreply.github.com>
2023-02-16 14:24:35 -0600

    test autostart

---
+++ Trial.py
@@ -0,0 +1,15 @@
+import time
+from datetime import datetime
+
+
+def writefile(k):
+    with open(f'logfiles/l{k}.txt', 'w') as file:
+        file.writelines(f'This is logfile{k} written at {str(datetime.now())}')
+        file.close()
+
+
+for i in range(10):
+    writefile(i)
+    time.sleep(5)
+
+print('Files written')
