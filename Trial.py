cb6f65a3acc4d876885e7aa4a77cb15f4468c769
Ashwin Ram <72587716+ashwinramise@users.noreply.github.com>
2023-02-16 14:28:55 -0600

    Update Trial.py

--- Trial.py
+++ Trial.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 def writefile(k):
-    with open(f'logfiles/l{k}.txt', 'w') as file:
+    with open(f'logfiles/l{k}.txt', 'w+') as file:
         file.writelines(f'This is logfile{k} written at {str(datetime.now())}')
         file.close()
 
