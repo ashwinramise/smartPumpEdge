3646d27d019afdedddffdb7f87866e1d69b19a80
Ashwin Ram <72587716+ashwinramise@users.noreply.github.com>
2023-07-25 07:05:43 -0500

    Update ModbusReadTrial.py

--- ModbusReadTrial.py
+++ ModbusReadTrial.py
@@ -4,14 +4,23 @@ from pymodbus.transaction import ModbusRtuFramer
 from datetime import datetime
 import json
 
+k = []
+with open("RegisterData.csv", "r") as csvfile:
+    reader_variable = csv.reader(csvfile, delimiter=",")
+    for row in reader_variable:
+        k.append(row)
+
+holding = [int(i[0]) for i in k[1:]]
+
 
 def getRegData(client, holds):
     mets = []
-    try:
-        out = client.read_holding_registers(address=holds, count=10, unit=1)
-        mets.append({str(holds): str(out.registers[0])})
-    except Exception as e:
-        print(e)
+    for val in holds:
+        try:
+            out = client.read_holding_registers(address=val, count=10, unit=1)
+            mets.append({str(val): str(out.registers[0])})
+        except Exception as e:
+            print(e)
     pingD = {
         'site': "test",
         'pump': "test",
@@ -23,5 +32,6 @@ def getRegData(client, holds):
 
 modbus_client = ModbusClient(method='rtu', port='/dev/ttymxc3', parity='N', baudrate=9600, stopbits=2, auto_open=True,
                              timeout=3)
-Regdata  = getRegData(modbus_client, 201)
+print(holding)
+Regdata = getRegData(modbus_client, holding)
 print(Regdata)
