b66822576c214e3c274c5d605680c4bdfbd14ce3
Ashwin Ram <72587716+ashwinramise@users.noreply.github.com>
2023-07-25 07:19:39 -0500

    Update ModbusReadTrial.py

--- ModbusReadTrial.py
+++ ModbusReadTrial.py
@@ -14,25 +14,26 @@ with open("RegisterData.csv", "r") as csvfile:
 holding = [int(i[0]) for i in k[1:]]
 
 
-def getRegData(client, holds):
-    mets = []
-    for val in holds:
-        try:
-            out = client.read_holding_registers(address=val, count=10, unit=1)
-            mets.append({str(val): str(out.registers[0])})
-        except Exception as e:
-            print(e)
-    pingD = {
-        'site': "test",
-        'pump': "test",
-        'timestamp': str(datetime.now()),
-        'metrics': mets
-    }
-    return pingD
+def getRegData(client, val):
+    try:
+        out = client.read_holding_registers(address=val, count=1, unit=1)
+        metric = {str(val): str(out.registers[0])}
+    except Exception as e:
+        print(e)
+
+    return metric
 
 
 modbus_client = ModbusClient(method='rtu', port='/dev/ttymxc3', parity='N', baudrate=9600, stopbits=2, auto_open=True,
                              timeout=3)
 print(holding)
-Regdata = getRegData(modbus_client, holding)
-print(Regdata)
+mets = []
+for reg in holding:
+    mets.append(getRegData(modbus_client, reg))
+pingD = {
+    'site': "test",
+    'pump': "test",
+    'timestamp': str(datetime.now()),
+    'metrics': mets
+}
+print(pingD)
