47346dc17347266071565be11fa53f666b776460
Ashwin Ram <72587716+ashwinramise@users.noreply.github.com>
2023-08-24 11:14:59 -0500

    Update pandas test.py

--- pandas test.py
+++ pandas test.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# regs = pd.read_csv('/root/smartPumpEdge/RegisterData.csv')  # 7970
-regs = pd.read_csv('RegisterData.csv') # windows
+regs = pd.read_csv('/root/smartPumpEdge/RegisterData.csv')  # 7970
+# regs = pd.read_csv('RegisterData.csv') # windows
 holding = regs['Address'].tolist()
 print(holding)
